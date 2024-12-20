eeab03d23522213b949ee46b2657c7da8bb4d806
Nath9666 <nathan.morel@efrei.net>
2024-12-20 02:59:44 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { StarryBackground } from "@/components/starry-background";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,7 +24,6 @@ export const metadata: Metadata = {
   keywords: "graphic design, digital art, illustration, Paris",
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -31,6 +31,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <Analytics />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
